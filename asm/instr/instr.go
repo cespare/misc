9916906245f01a115ed8e6a693e702ba49c9baf5
Caleb Spare <cespare@gmail.com>
2015-01-15 13:24:41 -0800

    Figuring out more stuff

--- asm/instr/instr.go
+++ asm/instr/instr.go
@@ -9,3 +9,15 @@ func Add2(n, m int64) int64
 // BSF returns the index of the least significant set bit,
 // or -1 if the input contains no set bits.
 func BSF(n int64) int
+
+func BSF32(n int32) int32
+
+func Sum(s []int64) int64 {
+	var ss int64
+	for _, n := range s {
+		ss += n
+	}
+	return ss
+}
+
+func Sum2(s []int64) int64
--- asm/main.go
+++ asm/main.go
@@ -5,6 +5,15 @@ import (
 )
 
 func main() {
-	s := instr.BSF(12)
+	//s := instr.Add2(5, 10)
+	//s := instr.BSF32(12)
+	ns := []int64{2, 3, 4}
+	s := instr.Sum(ns)
+	s2 := instr.Sum2(ns)
+	s3 := instr.Add(3, 4)
+	s4 := instr.Add2(3, 4)
 	println(s)
+	println(s2)
+	println(s3)
+	println(s4)
 }
