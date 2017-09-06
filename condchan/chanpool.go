a9705b703b616f28c706e1a815638c186c83b8f7
Bryan C. Mills <bcmills@google.com>
2017-09-06 14:58:47 -0400

    condchan: replace semaphore with a buffered channel

--- condchan/chanpool.go
+++ condchan/chanpool.go
@@ -3,42 +3,45 @@ package main
 import (
 	"context"
 	"sync"
-
-	"golang.org/x/sync/semaphore"
 )
+
+type token struct{}
 
 type ChanPool struct {
 	size int
-	sem  *semaphore.Weighted
+	sem  chan token // buffered to size; send to acquire
+	idle chan *Conn // buffered to size; send and receive with sem acquired
 
 	mu     sync.Mutex
-	idle   chan *Conn // buffered to size; closed with mu held when the pool is closed
-	closed bool
+	closed chan struct{} // closed with mu held when the pool is closed
 }
 
 func NewChanPool(size int) *ChanPool {
 	return &ChanPool{
-		size: size,
-		sem:  semaphore.NewWeighted(int64(size)),
-		idle: make(chan *Conn, size),
+		size:   size,
+		sem:    make(chan token, size),
+		idle:   make(chan *Conn, size),
+		closed: make(chan struct{}),
 	}
 }
 
 func (p *ChanPool) Get() (conn *Conn, err error) {
-	if err := p.sem.Acquire(context.TODO(), 1); err != nil {
-		return nil, err
+	ctx := context.TODO()
+	select {
+	case <-p.closed:
+		return nil, ErrPoolClosed
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case p.sem <- token{}:
 	}
 	defer func() {
 		if conn == nil {
-			p.sem.Release(1)
+			<-p.sem
 		}
 	}()
 
 	select {
-	case conn, ok := <-p.idle:
-		if !ok {
-			return nil, ErrPoolClosed
-		}
+	case conn = <-p.idle:
 		return conn, nil
 	default:
 		return Dial()
@@ -46,33 +49,29 @@ func (p *ChanPool) Get() (conn *Conn, err error) {
 }
 
 func (p *ChanPool) Put(c *Conn) {
-	defer p.sem.Release(1) // Wait to release until c is closed or returned.
-	if c.closed {
-		return
-	}
-
-	p.mu.Lock()
-	if p.closed {
-		defer c.Close()
-	} else {
+	if !c.closed {
 		p.idle <- c
 	}
-	p.mu.Unlock()
+	<-p.sem // Release after c is closed or returned.
 }
 
 func (p *ChanPool) Close() {
 	p.mu.Lock()
-	if !p.closed {
-		p.closed = true
-		close(p.idle)
+	select {
+	case <-p.closed:
+		p.mu.Unlock()
+		return
+	default:
+		close(p.closed)
 	}
 	p.mu.Unlock()
 
-	for conn := range p.idle {
-		conn.Close()
-	}
 	// Wait for all connections to be returned to the pool.
-	if err := p.sem.Acquire(context.TODO(), int64(p.size)); err == nil {
-		p.sem.Release(int64(p.size))
+	for n := p.size; n > 0; n-- {
+		p.sem <- token{}
+	}
+	close(p.idle)
+	for c := range p.idle {
+		c.Close()
 	}
 }
