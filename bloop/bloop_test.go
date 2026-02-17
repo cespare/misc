9cec9d37fa39495d1e1bce174976a3ab60e33d90
Caleb Spare <cespare@gmail.com>
2026-02-17 01:14:39 -0800

    Update the bloop

--- bloop/bloop_test.go
+++ bloop/bloop_test.go
@@ -1,25 +1,41 @@
 package bloop
 
-import "testing"
+import (
+	"testing"
+)
 
-func foo() int {
-	x := 123
-	for i := range 1000 {
-		x *= i
+func BenchmarkBaseline(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = expensive()
+	}
+}
+
+func BenchmarkSink(b *testing.B) {
+	var x float64
+	for i := 0; i < b.N; i++ {
+		x = expensive()
+	}
+	floatSink = x
+}
+
+func BenchmarkBLoop(b *testing.B) {
+	for b.Loop() {
+		expensive()
+	}
+}
+
+func BenchmarkBLoopAssign(b *testing.B) {
+	for b.Loop() {
+		_ = expensive()
+	}
+}
+
+var floatSink float64
+
+func expensive() float64 {
+	x := 1.0
+	for i := range int(1e6) {
+		x *= float64(i) * float64(i+1) * float64(i+2) * float64(i+3)
 	}
 	return x
-}
-
-func BenchmarkBasic(b *testing.B) {
-	for range b.N {
-		foo()
-	}
-}
-
-func BenchmarkSub(b *testing.B) {
-	b.Run("a", func(b *testing.B) {
-		for range b.N {
-			foo()
-		}
-	})
 }
